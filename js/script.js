e855be5d8e553de31c5c2b4b450d66f4b9b55daf
gitSuyog <codesuyog0@gmail.com>
2023-03-15 18:09:01 +0545

    Date Objects

--- js/script.js
+++ js/script.js
@@ -2092,3 +2092,54 @@
 // To stop the setInterval method we can use the clearInterval method
 //   }
 // }
+
+// # Date Objects
+// The Date object is used to work with dates and times
+// We just call the Date constructor.
+
+// Within the date constructor we can pass in the amount of milliseconds as arguments.
+// we can pass in year,month,day as arguments
+// let date = new Date (2023, 0, 1 , 2 ,3 ,4,5);
+// let date = new Date ("January 1, 2023 00:00:00");
+
+// We can get the current date by assigning it to a variable
+
+// let year = date.getFullYear();
+// let dayOfMonth = date.getDate();
+// let dayOfWeek = date.getDay();
+// let month = date.getMonth();
+// let hour = date.getHours();
+// let minutes = date.getMinutes();
+// let seconds = date.getSeconds();
+// let milliseconds = date.getMilliseconds();
+
+// We can set these properties as well
+// date.setFullYear(2024);
+
+// let date = new Date();
+// date = date.toLocaleString();
+// console.log(date);
+// document.getElementById("myLabel").innerHTML = date;
+// document.getElementById("myLabel").innerHTML = formatTime(date);
+
+// We can also create our own function to format Date
+
+// function formatDate(date) {
+//   let year = date.getFullYear();
+//   let month = date.getMonth();
+//   let day = date.getDate();
+
+//   return `${month}/${day}/${year}`;
+// }
+
+// function formatTime(date) {
+//   let hours = date.getHours();
+//   let minutes = date.getMinutes();
+//   let seconds = date.getSeconds();
+//   let amOrPm = hours >= 12 ? "pm" : "am";
+
+// To turn it into standard time
+//   hours = hours % 12 || 12;
+
+//   return `${hours}:${minutes}:${seconds} ${amOrPm}`;
+// }
