8a7816537c413a61a72460d4c3252d182d450126
gitSuyog <codesuyog0@gmail.com>
2023-03-15 18:20:02 +0545

    Clock Program

--- js/script.js
+++ js/script.js
@@ -2143,3 +2143,33 @@
 
 //   return `${hours}:${minutes}:${seconds} ${amOrPm}`;
 // }
+
+// Practice Program : Clock
+// const myLabel = document.getElementById("myLabel");
+
+// function update() {
+//   let date = new Date();
+//   myLabel.innerHTML = formatTime(date);
+
+//   function formatTime(date) {
+//     let hours = date.getHours();
+//     let minutes = date.getMinutes();
+//     let seconds = date.getSeconds();
+//     let amOrPm = hours >= 12 ? "pm" : "am";
+
+//     hours = hours % 12 || 12;
+//     hours = formatZero(hours);
+//     minutes = formatZero(minutes);
+//     seconds = formatZero(seconds);
+
+//     return `${hours}:${minutes}:${seconds} ${amOrPm}`;
+//   }
+
+//   function formatZero(time) {
+//     time = time.toString();
+//     return time.length < 2 ? "0" + time : time;
+//   }
+// }
+
+// update();
+// setInterval(update, 1000);
