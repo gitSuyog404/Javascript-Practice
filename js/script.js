789b0a7ef8ad23f3bc6fa7ecf6ee2ade7b2d51c7
gitSuyog <codesuyog0@gmail.com>
2023-03-20 20:07:36 +0545

    Canvas API basics

--- js/script.js
+++ js/script.js
@@ -2791,3 +2791,77 @@ Array.from(.children)
 //     }
 //   }
 // }
+
+// # Canvas API
+// It is a mean for drawing graphics used for animations, games, data visualizers
+
+// 5:50:54
+
+let canvas = document.getElementById("myCanvas");
+let context = canvas.getContext("2d");
+
+// context.strokeStyle = "purple";
+// context.lineWidth = 5;
+
+// context.beginPath();
+// context.moveTo(0, 0);
+// // context.lineTo(500, 500);
+// context.lineTo(250, 250);
+// context.lineTo(250, 500);
+// context.moveTo(500, 0);
+// context.lineTo(250, 250);
+
+// ! Drawing a Triangle
+// context.stroke();
+// context.strokeStyle = "aqua";
+// context.fillStyle = "tomato";
+// context.lineWidth = 10;
+// context.beginPath();
+// context.moveTo(250, 0);
+// context.lineTo(0, 250);
+// context.lineTo(500, 250);
+// context.lineTo(250, 0);
+// context.stroke();
+// context.fill();
+
+// Drawing a rectangle;
+// context.fillStyle = "greenyellow";
+// context.fillRect(0, 0, 250, 250);
+// context.stroke();
+// context.strokeStyle = "black";
+// context.strokeRect(0, 0, 250, 250);
+
+// context.fillStyle = "yellow";
+// context.fillRect(0, 250, 250, 250);
+// context.stroke();
+// context.strokeStyle = "black";
+// context.strokeRect(0, 250, 250, 250);
+
+// context.fillStyle = "black";
+// context.fillRect(250, 250, 250, 250);
+// context.stroke();
+// context.strokeStyle = "black";
+// context.strokeRect(250, 250, 250, 250);
+
+// context.fillStyle = "yellow";
+// context.fillRect(250, 0, 250, 250);
+// context.stroke();
+// context.strokeStyle = "black";
+// context.strokeRect(250, 0, 250, 250);
+
+// Drawing sircle
+// (x,y,r,sAngle,eAngle,counterclockwise)
+
+// context.fillStyle = "lightblue";
+// context.lineWidth = 10;
+// context.strokeStyle = "darkblue";
+// context.beginPath();
+// context.arc(250, 250, 200, 0, 2 * Math.PI);
+// context.stroke();
+// context.fill();
+
+// ! Draw Text
+// context.font = "50px MV Boli";
+// context.fillStyle = "grey";
+// context.textAlign = "center";
+// context.fillText("JAVASCRIPT", canvas.width / 2, canvas.height / 2);
