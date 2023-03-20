8e5b7ff877f870ddce6f1f8eb55e2c6462c9efdd
gitSuyog <codesuyog0@gmail.com>
2023-03-20 16:41:21 +0545

    Javascript Animations Basics

--- js/script.js
+++ js/script.js
@@ -2673,3 +2673,121 @@ Array.from(.children)
 //       break;
 //   }
 // }
+
+// # Javascript Animations
+
+// const myButton = document.getElementById("myButton");
+// const myAnimation = document.getElementById("myDiv");
+// myButton.addEventListener("click", start);
+
+// function start() {
+//   let timerId = null;
+//   let x = 0;
+//   let y = 0;
+
+//   timerId = setInterval(frame, 5);
+
+//   function frame() {
+// horizontal
+// if (x >= 200) {
+//   clearInterval(timerId);
+// } else {
+//   x += 1;
+//   myAnimation.style.left = x + "px";
+// }
+
+// vertical
+// if (y >= 200) {
+//   clearInterval(timerId);
+// } else {
+//   y += 1;
+//   myAnimation.style.top = y + "px";
+// }
+
+// Both vertical and horizontal
+
+//     if (x >= 200 || y >= 200) {
+//       clearInterval(timerId);
+//     } else {
+//       x += 1;
+//       y += 1;
+//       myAnimation.style.left = x + "px";
+//       myAnimation.style.top = y + "px";
+//     }
+//   }
+// }
+
+// Making it rotate
+
+// const myButton = document.getElementById("myButton");
+// const myAnimation = document.getElementById("myDiv");
+// myButton.addEventListener("click", start);
+
+// function start() {
+//   let timerId = null;
+//   let degrees = 0;
+
+//   timerId = setInterval(frame, 5);
+//   function frame() {
+//     if (degrees >= 360) {
+//       clearInterval(timerId);
+//     } else {
+//       degrees += 1;
+// myAnimation.style.transform = "rotateX(" + degrees + "deg)";
+// myAnimation.style.transform = "rotateY(" + degrees + "deg)";
+//       myAnimation.style.transform = "rotateZ(" + degrees + "deg)";
+//     }
+//   }
+// }
+
+// Combining all of it as practice
+
+// const myButton = document.getElementById("myButton");
+// const myAnimation = document.getElementById("myDiv");
+// myButton.addEventListener("click", start);
+
+// function start() {
+//   let timerId = null;
+//   let degrees = 0;
+//   let x = 0;
+//   let y = 0;
+
+//   timerId = setInterval(frame, 5);
+//   function frame() {
+//     if (x >= 200 || y >= 200) {
+//       clearInterval(timerId);
+//     } else {
+//       degrees += 5;
+//       x += 1;
+//       y += 1;
+
+//       myAnimation.style.left = x + "px";
+//       myAnimation.style.top = y + "px";
+//       myAnimation.style.transform = "rotateZ(" + degrees + "deg)";
+//     }
+//   }
+// }
+
+// We can also scale the size of an image
+
+// const myButton = document.getElementById("myButton");
+// const myAnimation = document.getElementById("myDiv");
+// myButton.addEventListener("click", start);
+
+// function start() {
+//   let timerId = null;
+//   let scaleX = 1;
+//   let scaleY = 1;
+
+//   timerId = setInterval(frame, 5);
+
+//   function frame() {
+//     if (scaleX >= 2 || scaleY >= 2) {
+//       clearInterval(timerId);
+//     } else {
+//       scaleX += 0.01;
+//       scaleY += 0.01;
+//       myAnimation.style.transform = "scale(" + scaleX + ", " + scaleY + ")";
+//     }
+//   }
+// }
